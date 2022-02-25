30c7c35938474585b7685bc20532da94035e190c
949027 <82878668+949027@users.noreply.github.com>
2022-02-25 14:03:08 +0700

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, Image
 
 
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description_short',)
+    list_display = ('title', 'description_short', )
 
 
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Image, ImageAdmin)
 admin.site.register(Place, PlaceAdmin)
-
--- places/models.py
+++ places/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField('Наименование', max_length=200)
-    imgs = models.ImageField('Изображения')
-    description_short = models.CharField('Краткое описание', max_length=200)
+    description_short = models.CharField('Краткое описание', max_length=400)
     description_long = models.TextField('Подробное описание')
     lng = models.FloatField()
     lat = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    number = models.IntegerField('Номер')
+    place = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='images', verbose_name='Место')
+    file = models.ImageField('Изображения', blank=True)
+
+    def __str__(self):
+        return self.number, self.place
