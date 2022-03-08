0a8d1b9426c24efa65b22cab7675d13a48ef4a79
949027 <949027@gmail.com>
2022-03-08 20:22:58 +0700

    Improved photo adding

--- places/admin.py
+++ places/admin.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from .models import Place, Image
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('title', 'description_short', )
+    inlines = [
+        ImageInline,
+    ]
 
 
 class ImageAdmin(admin.ModelAdmin):
