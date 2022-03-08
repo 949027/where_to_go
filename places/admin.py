7e9293aea5b1a2e360adbd246df4654e7c40e79e
949027 <949027@gmail.com>
2022-03-08 21:46:26 +0700

    Added preview photo

--- places/admin.py
+++ places/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
+
     model = Image
+    readonly_fields = [
+        'headshot_image',
+    ]
+
+    def headshot_image(self, obj):
+        return format_html(
+            '<img src="{}" width="{}" height={} />',
+            obj.file.url,
+            200,
+            obj.file.height * 200 / obj.file.width,
+        )
 
 
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description_short', )
+    list_display = ('title', 'description_short',)
     inlines = [
         ImageInline,
     ]
@@ -15,6 +28,7 @@ class PlaceAdmin(admin.ModelAdmin):
 
 class ImageAdmin(admin.ModelAdmin):
     pass
+
 
 admin.site.register(Image, ImageAdmin)
 admin.site.register(Place, PlaceAdmin)
